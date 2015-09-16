d9fadedd3af8b93de52990138d658c360a2a30f1
samuso <sam.fallahi@gmail.com>
2015-09-16 14:42:27 +0100

    added function to automate process

--- CoordinateGen.py
+++ CoordinateGen.py
@@ -1,3 +1,9 @@
+# initialise and then use the
+# withAngle method to generate a
+# list of 2 tuples with the
+# coordinates of the final points
+# coordinateGen = CoordinateGen(startPoint, endPoint, rootSectionLength, middleSectionLength, endSectionLength)
+# solution = coordinateGen.withAngle(minimumAngle)
 from __future__ import division
 import math
 
@@ -33,3 +39,25 @@ class CoordinateGen:
         xDistance = pointOne[0] - pointTwo[0]
         yDistance = pointOne[1] - pointTwo[1]
         return math.sqrt(xDistance**2 + yDistance**2)
+
+    def withAngle(self, angle):
+        # first we generate the first point
+        pointOne = self.getPointOnCircle(self._startingPoint, self._rootSectionLength, angle)
+
+        # then we need two angles
+        # angle one
+        l1 = self.distanceBetweenPoints(pointOne, self._objectPoint)
+        l2 = self._endSectionLength
+        l3 = self._middleSectionLength
+        angle1 = self.triangleAngleCalculator(l1, l2, l3)
+        # angle2
+        l1 = l1
+        l2 = self.distanceBetweenPoints(self._objectPoint, self._startingPoint)
+        l3 = self._rootSectionLength
+        angle2 = self.triangleAngleCalculator(l1, l2, l3)
+        # now we get the total of the two angles
+        totalAngle = math.pi - (angle1 + angle2)
+
+        # and finally the second point
+        pointTwo =  self.getPointOnCircle(self._objectPoint, self._endSectionLength, totalAngle)
+        return [pointOne, pointTwo]
