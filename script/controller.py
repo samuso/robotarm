e405b54a8dea81c590719a47ebc73bad9e71942a
Chinemelu Ezeh <chinemeluezeh@gmail.com>
2015-09-17 18:34:29 +0100

    Joint1 working

--- script/controller.py
+++ script/controller.py
@@ -9,17 +9,20 @@ import struct
 import numpy as np, math
 
 import serial
+from CoordinateGen import CoordinateGen
 
 STARTSEQ = chr(108)
-
+rootSectionLength = 98
+middleSectionLength = 122
+endSectionLength = 75
 
 class robotarm(object):
 	def __init__(self):
-		import atexit
+		self.coordinateGen = CoordinateGen()
+
 		self.initialiseVision()
-		atexit.register(self.exit_fn)
 		self.ser = serial.Serial(
-			port='/dev/tty.usbmodem142141',
+			port='/dev/tty.usbmodem1411',
 			baudrate=115200,
 			parity=serial.PARITY_NONE,
 			stopbits=serial.STOPBITS_ONE,
@@ -58,22 +61,66 @@ class robotarm(object):
 				# 	cmd = chr(0B00110000)
 				# 	self.ser.write(cmd)
 
-				#tracking a0
-				a = self.getdist( base, joint0)
-				b = self.getdist(base, obj)
-				c = self.getdist( obj, joint0)
-				ja0 = np.arccos((a**2 + b**2 - c**2)/(2*a*b))
+				#tracking ba0
+				# a = self.getdist( base, joint0)
+				# b = self.getdist(base, obj)
+				# c = self.getdist( obj, joint0)
+				# ba0 = np.arccos((a**2 + b**2 - c**2)/(2*a*b))
 
-				ja0_target = np.pi*2/3
-				if (ja0_target - ja0) > .2:
-					# base_0 go forward
+				
+				# ba0_target = np.pi*2/3
+
+				# if (ba0_target - ja0) > .2:
+				# 	# base_0 go forward
+				# 	self.ser.write(STARTSEQ)
+				# 	cmd = chr(0B01010000)
+				# 	self.ser.write(cmd)
+				# elif (ba0_target - ja0) < -.2:
+				# 	#base_0 go backward
+				# 	self.ser.write(STARTSEQ)
+				# 	cmd = chr(0B01110000)
+				# 	self.ser.write(cmd)
+
+				#tracking ja0
+				# ba0 = np.pi *2/3
+				# ja0_target = self.coordinateGen.withAngle(base, obj, rootSectionLength, middleSectionLength, endSectionLength, ba0)
+				# print "ja0_target: {}".format(ja0_target)
+				# a = self.getdist( base, joint0)
+				# b = self.getdist(joint0, joint1)
+				# c = self.getdist( base, joint1)
+				# ja0 = np.arccos((a**2 + b**2 - c**2)/(2*a*b))
+
+
+				# if (ja0_target - ja0) > .2:
+				# 	# base_0 go forward
+				# 	self.ser.write(STARTSEQ)
+				# 	cmd = chr(0B10010000)
+				# 	self.ser.write(cmd)
+				# elif(ja0_target - ja0) < -.2:
+				# 	#base_0 go backward
+				# 	self.ser.write(STARTSEQ)
+				# 	cmd = chr(0B10110000)
+				# 	self.ser.write(cmd)
+
+				#Tracking ja1
+				l2angle = np.arctan2(joint1[1]-joint0[1], joint1[0]-joint0[0])
+
+				ja1 = np.pi + ( joint1_roll - l2angle)
+
+				ja1_target = np.pi
+
+				print "ja1_target: {}, ja1: {}, l2angle:{}, joint1_roll:{}".format(ja1_target, ja1, l2angle, joint1_roll)
+				if (ja1_target - ja1) > .2:
+					print "backward"
+					# joint1 go backward
 					self.ser.write(STARTSEQ)
-					cmd = chr(0B01010000)
+					cmd = chr(0B11110000)
 					self.ser.write(cmd)
-				elif (ja0_target - ja0) < -.2:
-					#base_0 go backward
+				elif(ja1_target - ja1) < -.2:
+					print "forward"
+					#joint1 go forward
 					self.ser.write(STARTSEQ)
-					cmd = chr(0B01110000)
+					cmd = chr(0B11010000)
 					self.ser.write(cmd)
 
 	def getDistFromPlane (self, p1, p2, p3, pt):
@@ -103,7 +150,7 @@ class robotarm(object):
 	def initialiseVision(self):
 		# start vision
 		cmd = "./../build/getcoords"
-		self.proc = subprocess.Popen(cmd, preexec_fn=os.setsid)
+		self.proc = subprocess.Popen(cmd,shell=False, preexec_fn=os.setsid)
 		time.sleep(3)
 		self.pipein = open("/tmp/autoarm",'r')
 
@@ -114,9 +161,21 @@ class robotarm(object):
 		# print "x1: {}, y1: {}, z1:{}\n x2:{},y2:{},z2:{}, \n x3:{},y3:{},z3:{} \n x4:{},y4:{},z4:{}, roll: {}".format(x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4,r)
 		return -x1,-y1,z1,-x2,-y2,z2,-x3,-y3,z3,-x4,-y4,z4,r
 
-	def exit_fn(self):
-		self.pipein.close()
-		self.proc.terminate()
-		
+	def __del__(self):
+		try:
+			self.pipein.close()
+		except:
+			pass
+		print "close please!"
+		try:
+			os.killpg(self.proc.pid, signal.SIGTERM)
+		except:
+			pass
+		try:
+			ser.close()
+		except:
+			pass
+	def initialiseRoll(self):
+		a = np.arctan2(joint1[1]-joint2[1])
 if __name__ == "__main__":
 	robotarm()
