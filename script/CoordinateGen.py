9ea86517d1f5b4b53b34d758eece9c0e4b2010e2
samuso <sam.fallahi@gmail.com>
2015-09-17 15:16:30 +0100

    changes applied to CoordinateGen in scripts

--- script/CoordinateGen.py
+++ script/CoordinateGen.py
@@ -2,19 +2,15 @@
 # withAngle method to generate a
 # list of 2 tuples with the
 # coordinates of the final points
-# coordinateGen = CoordinateGen(startPoint, endPoint, rootSectionLength, middleSectionLength, endSectionLength)
-# solution = coordinateGen.withAngle(minimumAngle)
+# coordinateGen = CoordinateGen()
+# solution = coordinateGen.withAngle(startPoint, endPoint, rootSectionLength, middleSectionLength, endSectionLength, minimumAngle)
 from __future__ import division
 import math
 
 class CoordinateGen:
 
-    def __init__ (self, startingPoint, objectPoint, rootSectionLength, middleSectionLength, endSectionLength):
-        self._startingPoint = startingPoint;
-        self._objectPoint = objectPoint;
-        self._rootSectionLength = rootSectionLength;
-        self._middleSectionLength = middleSectionLength;
-        self._endSectionLength = endSectionLength;
+    def __init__ (self):
+        pass
 
     def _isTriangle(self, side1, side2, side3):
         if (side1 + side2 < side3 or
@@ -40,24 +36,24 @@ class CoordinateGen:
         yDistance = pointOne[1] - pointTwo[1]
         return math.sqrt(xDistance**2 + yDistance**2)
 
-    def withAngle(self, angle):
+    def withAngle(self, startingPoint, objectPoint, rootSectionLength, middleSectionLength, endSectionLength, angle):
         # first we generate the first point
-        pointOne = self.getPointOnCircle(self._startingPoint, self._rootSectionLength, angle)
+        pointOne = self.getPointOnCircle(startingPoint, rootSectionLength, angle)
 
         # then we need two angles
         # angle one
-        l1 = self.distanceBetweenPoints(pointOne, self._objectPoint)
-        l2 = self._endSectionLength
-        l3 = self._middleSectionLength
+        l1 = self.distanceBetweenPoints(pointOne, objectPoint)
+        l2 = endSectionLength
+        l3 = middleSectionLength
         angle1 = self.triangleAngleCalculator(l1, l2, l3)
         # angle2
         l1 = l1
-        l2 = self.distanceBetweenPoints(self._objectPoint, self._startingPoint)
-        l3 = self._rootSectionLength
+        l2 = self.distanceBetweenPoints(objectPoint, startingPoint)
+        l3 = rootSectionLength
         angle2 = self.triangleAngleCalculator(l1, l2, l3)
         # now we get the total of the two angles
         totalAngle = math.pi - (angle1 + angle2)
 
         # and finally the second point
-        pointTwo =  self.getPointOnCircle(self._objectPoint, self._endSectionLength, totalAngle)
+        pointTwo =  self.getPointOnCircle(objectPoint, endSectionLength, totalAngle)
         return pointTwo
